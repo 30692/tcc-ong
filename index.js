a1710effbbd97faa46d24c820ac708bc8439b43e
30692 <Felipeh.albuquerque@gmail.com>
2020-03-12 20:08:48 -0300

    Pagina index

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const rotasDeUsuario = require("./src/routes/usuarios");
 const rotasDePatrocinador = require("./src/routes/patrocinadores");
 const rotasDeOng = require("./src/routes/ongs");
+const nunjucks = require('nunjucks');
 const app = express();
 
 app.use(cors());
@@ -13,6 +14,18 @@ app.use(
     extended: false
   })
 );
+
+nunjucks.configure('views', {
+  express: app
+})
+
+app.get('/', function(req,res) {
+  res.render('./index.html')
+});
+
+app.get('/blog', function(req,res) {
+  res.render('./blog.html')
+})
 
 app.use("/usuarios", rotasDeUsuario);
 app.use("/patrocinadores", rotasDePatrocinador);
